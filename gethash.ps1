8647708291c7750955906605a80464728979ee4d
notisrac <notisrac@gmail.com>
2015-07-26 00:56:59 +0200

    Fixed: gethash.ps1 path separators

--- gethash.ps1
+++ gethash.ps1
@@ -5,7 +5,7 @@ $fileList = Get-FileHash Background\*.* -Algorithm MD5
 foreach($line in $fileList)
 {
 	$path = $line.Path
-	$path = $path.replace("$workdir\", "")
+	$path = $path.replace("$workdir\", "").replace("\", "/")
 	$hash = $line.Hash
 	"$path, $hash" | Out-File $outFile -Encoding "utf8" -Append
 }
