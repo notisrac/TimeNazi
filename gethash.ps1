b3a599b7b0e00fd3534f2dc2d7ef07c054e2add7
notisrac <notisrac@gmail.com>
2015-07-26 00:54:37 +0200

    Files.list is now in utf8
     - Fixed: index.html files.list path, and empty rows are no longer processed
     - Fixed: gethash.ps1 now outputs utf8

--- gethash.ps1
+++ gethash.ps1
@@ -1,1 +1,11 @@
-Get-FileHash Background\*.* -Algorithm MD5 | % { Write-Host "$($_.Path), $($_.Hash)" } > files.list
+$outFile = "files.list"
+Remove-Item $outFile
+$workdir = pwd
+$fileList = Get-FileHash Background\*.* -Algorithm MD5
+foreach($line in $fileList)
+{
+	$path = $line.Path
+	$path = $path.replace("$workdir\", "")
+	$hash = $line.Hash
+	"$path, $hash" | Out-File $outFile -Encoding "utf8" -Append
+}
